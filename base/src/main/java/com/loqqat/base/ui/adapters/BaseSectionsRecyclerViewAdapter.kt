64fadcfa23788aa5dcd89b2ab2734ccacf3bfafc
vimal_wddh8uf <vimal@qaptive.co.in>
2019-11-12 17:43:34 +0530

    utility methods added

--- base/src/main/java/com/loqqat/base/ui/adapters/BaseSectionsRecyclerViewAdapter.kt
+++ base/src/main/java/com/loqqat/base/ui/adapters/BaseSectionsRecyclerViewAdapter.kt
@@ -3,23 +3,38 @@ package com.loqqat.base.ui.adapters
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class BaseSectionsRecyclerViewAdapter<KEY, DATA, VH : RecyclerView.ViewHolder>(var data: MutableMap<KEY?, List<DATA>> = mutableMapOf()):RecyclerView.Adapter<VH>() {
+/**
+ * Base Class for RecyclerView with sections, all the general handling for adapter are done here, Like adding , appending ,removing Items etc.
+ * A 2 dimension array is created based on Key amd Data , each row is treated as a section
+ * @param KEY Data type of key or header data
+ * @param DATA Data type or model of adapter holding , an ArrayList<T> will be crated
+ * @param VH ViewHolder of the adapter
+ * @property data initial data set for the adapter
+ *@constructor Creates an RecyclerView.Adapter with provided data
+ */
+abstract class BaseSectionsRecyclerViewAdapter<KEY, DATA, VH : RecyclerView.ViewHolder>(var data: MutableMap<KEY?, List<DATA>> = mutableMapOf()) :
+    RecyclerView.Adapter<VH>() {
 
     var emptyView: View? = null
     var progressBar: View? = null
     var errorView: View? = null
 
-    object ViewType{
-        val HEADER=1
-        val ITEM=0
+    /**
+     * Type of view
+     */
+    object ViewType {
+        val HEADER = 1
+        val ITEM = 0
     }
 
 
+    /**
+     * calculates total number of items in recycler view including headers
+     */
     final override fun getItemCount(): Int {
-        var count=0
-        for (set in data)
-        {
-            count+=set.value.size
+        var count = 0
+        for (set in data) {
+            count += set.value.size
             count++
         }
         emptyView?.visibility = if (count == 0 && progressBar?.visibility ?: View.GONE == View.GONE)
@@ -30,18 +45,16 @@ abstract class BaseSectionsRecyclerViewAdapter<KEY, DATA, VH : RecyclerView.View
     }
 
     final override fun onBindViewHolder(holder: VH, position: Int) {
-        var count=0
-        for(set in data)
-        {
-            if(count==position) {
-                bindHeader(set.key,holder)
+        var count = 0
+        for (set in data) {
+            if (count == position) {
+                bindHeader(set.key, holder)
                 return
             }
             count++
-            for(item in set.value)
-            {
-                if(position==count) {
-                    bindItem(item,holder)
+            for (item in set.value) {
+                if (position == count) {
+                    bindItem(item, holder)
                     return
                 }
                 count++
@@ -50,20 +63,19 @@ abstract class BaseSectionsRecyclerViewAdapter<KEY, DATA, VH : RecyclerView.View
     }
 
     final override fun getItemViewType(position: Int): Int {
-        var count=0
-        for(set in data)
-        {
-            if(count==position)
+        var count = 0
+        for (set in data) {
+            if (count == position)
                 return ViewType.HEADER
             count++
-            count+=set.value.size
-            if(position<count)
+            count += set.value.size
+            if (position < count)
                 return ViewType.ITEM
         }
         return ViewType.ITEM
     }
 
-    /*
+    /**
        For showing loader in timeline and contact fragment only
      */
     open fun showLoading() {
@@ -76,10 +88,10 @@ abstract class BaseSectionsRecyclerViewAdapter<KEY, DATA, VH : RecyclerView.View
     open fun hideLoading() {
         progressBar?.visibility = View.GONE
         errorView?.visibility = View.GONE
-        emptyView?.visibility = if (data?.size == 0) View.VISIBLE else View.GONE
+        emptyView?.visibility = if (data.isEmpty()) View.VISIBLE else View.GONE
     }
 
-    /*
+    /**
     For showing error view in timeline and contact fragment only
   */
     open fun showError() {
@@ -93,7 +105,7 @@ abstract class BaseSectionsRecyclerViewAdapter<KEY, DATA, VH : RecyclerView.View
      * Set data set
      * @param data data to be set
      */
-    open fun setItems(data:  Map<KEY?, List<DATA>>?) {
+    fun setItems(data: Map<KEY?, List<DATA>>?) {
         this.data.clear()
         if (data != null) {
             this.data.putAll(data)
@@ -107,11 +119,48 @@ abstract class BaseSectionsRecyclerViewAdapter<KEY, DATA, VH : RecyclerView.View
      * @param data cleared
      */
 
-    open fun clearData() {
+    fun clearData() {
         data.clear()
         notifyDataSetChanged()
     }
 
-    abstract fun bindHeader(key:KEY?, holder: VH)
-    abstract fun bindItem(item:DATA?,holder: VH)
+    fun addRow(key: KEY,data:List<DATA>){
+        this.data[key] = data
+    }
+
+    /**
+     * Removes the specified key and its corresponding value from this map.
+     *
+     * @return the previous value associated with the key, or `null` if the key was not present in the map.
+     */
+    fun removeRow(key: KEY):List<DATA>?
+    {
+        return this.data.remove(key)
+    }
+
+    fun getHeaderCount():Int
+    {
+        return data.size
+    }
+
+    fun getChildCount():Int
+    {
+        return data.values.size
+    }
+
+    /**
+     * abstract method to bind data of header holder and data is provided based on position
+     * header view bindings can be done here
+     * @param key data in header
+     * @param holder viewHolder of header
+     */
+    abstract fun bindHeader(key: KEY?, holder: VH)
+
+    /**
+     * abstract method to bind data of child holder and data is provided based on position
+     * child view bindings can be done here
+     * @param item data to bind
+     * @param holder ViewHolder of child
+     */
+    abstract fun bindItem(item: DATA?, holder: VH)
 }
